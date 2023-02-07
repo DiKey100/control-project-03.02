f526258de6786892598c7456c528a9e3b16437fb
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-02-07 21:44:16 +0200

    add logo

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,10 +1,10 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/material/Container'
 import Menu from 'components/Menu/Menu'
+import Logo from 'container/Logo/Logo'
 
 type Props = {}
 
@@ -23,13 +23,7 @@ const Header = (props: Props) => {
                         >
                             <MenuIcon />
                         </IconButton>
-                        <Typography
-                            variant="h6"
-                            component="div"
-                            sx={{ flexGrow: 1 }}
-                        >
-                            News
-                        </Typography>
+                        <Logo />
                         <Menu />
                     </Toolbar>
                 </Container>
