642d4ea08712a58e9911196fa3086c65f293b1ae
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-02-08 05:48:12 +0200

    remove unnecessary code and add sass on project

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,7 +1,5 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/material/Container'
 import Menu from 'components/Menu/Menu'
 import Logo from 'container/Logo/Logo'
@@ -14,15 +12,6 @@ const Header = (props: Props) => {
             <AppBar position="static">
                 <Container maxWidth="lg">
                     <Toolbar>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
                         <Logo />
                         <Menu />
                     </Toolbar>
