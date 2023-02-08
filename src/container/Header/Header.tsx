98c4c3f2a41e27c54c54053c980a88b1af417af7
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-02-08 06:14:38 +0200

    create Header.scss

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -3,6 +3,7 @@ import Toolbar from '@mui/material/Toolbar'
 import Container from '@mui/material/Container'
 import Menu from 'components/Menu/Menu'
 import Logo from 'container/Logo/Logo'
+import './Header.scss'
 
 type Props = {}
 
