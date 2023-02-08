8b6466ce595c8f6258ad4154b52059cbdb7340ae
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-02-09 01:03:32 +0200

    connection Home in Main

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,7 +1,14 @@
+import { Container } from '@mui/material'
+import Home from 'pages/Home/Home'
+
 type Props = {}
 
 const Main = (props: Props) => {
-    return <div>Main</div>
+    return (
+        <Container>
+            <Home />
+        </Container>
+    )
 }
 
 export default Main
