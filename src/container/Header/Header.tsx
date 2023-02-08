db39d788bcc3d0041792638ecff00ef65f65f7d4
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-02-09 00:12:07 +0200

    second variant add classes in .tsx

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -3,14 +3,14 @@ import Toolbar from '@mui/material/Toolbar'
 import Container from '@mui/material/Container'
 import Menu from 'components/Menu/Menu'
 import Logo from 'container/Logo/Logo'
-import './Header.scss'
+import classes from './Header.module.css'
 
 type Props = {}
 
 const Header = (props: Props) => {
     return (
         <>
-            <AppBar position="static" className="app-bar">
+            <AppBar position="static" className={`${classes['app-bar']}`}>
                 <Container maxWidth="lg">
                     <Toolbar>
                         <Logo />
