1e9b2f52119e70e3a912ee761447fae0b9fde6d7
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-02-10 13:54:25 +0200

    add array with products in project

---
+++ src/utils/productsArray.ts
@@ -0,0 +1,33 @@
+type ProductProps = {
+    title: string
+    price: number
+}
+
+const productsArray:ProductProps[] = [
+    {
+        title: "Кроссовки FILA",
+        price: 600
+    },
+    {
+        title: "Классические кеды",
+        price: 400
+    },
+    {
+        title: "Кроссовки Fagaoge",
+        price: 700
+    },
+    {
+        title: "Кроссовки S",
+        price: 450
+    },
+    {
+        title: "Кроссовки чёрно-белые",
+        price: 600
+    },
+    {
+        title: "Кроссовки Nike Air",
+        price: 800
+    }
+]
+
+export default productsArray
