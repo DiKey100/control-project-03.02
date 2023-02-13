dcdf4f117aca9443f2fd6fa7aedd94a50f48c35e
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-02-13 19:31:37 +0200

    add CartData in App.tsx

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -2,10 +2,21 @@ import { StyledEngineProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
+import { useState } from 'react'
 
 type Props = {}
 
+type CartData = {
+    totalCount: number
+    totalPrice: number
+}
+
 const App = (props: Props) => {
+    const [cartData, setCartData] = useState<CartData>({
+        totalCount: 0,
+        totalPrice: 0,
+    })
+
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
