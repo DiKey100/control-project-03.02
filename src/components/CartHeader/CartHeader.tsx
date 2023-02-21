7672c2224ba8b3da230af56a2c6da8704bb96f63
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-02-21 19:51:16 +0200

    add shortCart in Header. And add fitst style in ShCart

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import productsArray, {
     getProductsObject,
     ProductProps,
 } from 'utils/productsArray'
+import './CartHeader.scss'
 
 type Props = {
     whatProductsInCart: {
@@ -16,27 +18,57 @@ const CartHeader = ({
     whatProductsInCart,
     productsObject = getProductsObject(productsArray),
 }: Props) => {
+    const [isShortCartShow, setIsShortCartShow] = useState<boolean>(false)
     return (
         <div>
-            <div>
-                {Object.keys(whatProductsInCart).map((productId) => (
-                    <div key={productId}>
-                        {productsObject[parseInt(productId)].title} :{' '}
-                        {whatProductsInCart[parseInt(productId)]}
+            <button
+                onClick={() => setIsShortCartShow(true)}
+                className="open-sh-cart"
+            >
+                Показать корзину
+            </button>
+            {isShortCartShow && (
+                <div className="sh-cart">
+                    <div
+                        onClick={() => setIsShortCartShow(false)}
+                        className="close-sh-cart"
+                    >
+                        X
                     </div>
-                ))}
-            </div>
-            <div>
-                Общая сумма:{' '}
-                {Object.keys(whatProductsInCart).reduce(
-                    (total, productId) =>
-                        total +
-                        whatProductsInCart[parseInt(productId)] *
-                            productsObject[parseInt(productId)].price,
-                    0
-                )}{' '}
-                гривен
-            </div>
+                    <div className="sh-cart-info">
+                        <div>
+                            {Object.keys(whatProductsInCart).map(
+                                (productId) => (
+                                    <div key={productId}>
+                                        {
+                                            productsObject[parseInt(productId)]
+                                                .title
+                                        }{' '}
+                                        :{' '}
+                                        {
+                                            whatProductsInCart[
+                                                parseInt(productId)
+                                            ]
+                                        }
+                                    </div>
+                                )
+                            )}
+                        </div>
+                        <div>
+                            Общая сумма:{' '}
+                            {Object.keys(whatProductsInCart).reduce(
+                                (total, productId) =>
+                                    total +
+                                    whatProductsInCart[parseInt(productId)] *
+                                        productsObject[parseInt(productId)]
+                                            .price,
+                                0
+                            )}{' '}
+                            гривен
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
