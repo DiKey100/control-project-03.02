64930d02d03f539697e21a7a5a6aa50d51014f16
Daniel Kuznecov <daniilavto2017@gmail.com>
2023-03-13 19:59:35 +0200

    add Slider in Home. Start change style Slider. not fixed error with absence some img

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,10 +2,12 @@ import { Typography } from '@mui/material'
 import FilterMenu from 'components/FilterMenu/FilterMenu'
 import ProductsList from 'components/Products/ProductsList'
 import Toolbar from '@mui/material/Toolbar'
+import Slider from 'components/Slider/Slider'
 
 const Home = () => {
     return (
         <>
+            <Slider />
             <Typography
                 variant="h4"
                 component="h1"
